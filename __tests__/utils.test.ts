cdba5e46c10c6ec43b9a38fd5ead6060a228be7f
huynguyen <huynguyen@freec.asia>
2023-09-28 11:30:03 +0700

    Build project with fail run test

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -10,7 +10,8 @@ describe('TESTING UTILS FUNCTIONS', () => {
 
     it('toUppercase => Should return a string has been uppercase', () => {
         const str = toUppercase('uppercase words');
-        expect(str).not.toBeNull();
+        //expect(str).not.toBeNull();
+        expect(str).toBeNull();
         expect(str).not.toBe(undefined);
         expect(str).toEqual('UPPERCASE WORDS');
     })
