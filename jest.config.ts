33229a5cddccb1c3c6f72a4bcdda15751c3cd086
huynguyen <huynguyen@freec.asia>
2023-09-28 18:04:34 +0700

    Update prettier, eslint for run lint step in github action

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,19 @@
-import type { Config } from '@jest/types';
+import { Config } from "@jest/types"
 
-const baseDir = '<rootDir>/src/';
+const baseDir = "<rootDir>/src/"
 
 const jestConfig: Config.InitialOptions = {
-    transform: {
-        '^.+\\.ts?$': 'ts-jest',
-    },
-    testEnvironment: 'node',
-    verbose: true,
-    clearMocks: true,
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageProvider: 'v8',
-    collectCoverageFrom: [
-        `${baseDir}/**/*.ts`
-    ],
-    testMatch: [
-        `**/__tests__/**/*.+(ts|tsx|js)`,
-    ],
-};
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  testEnvironment: "node",
+  verbose: true,
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  collectCoverageFrom: [`${baseDir}/**/*.ts`],
+  testMatch: [`**/__tests__/**/*.+(ts|tsx|js)`],
+}
 
-export default jestConfig;
+export default jestConfig
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,15 @@
-export function toLowerCase(str: string = ''): string | undefined {
-    return str.toLowerCase();
+export function toLowerCase(str: string = ""): string | undefined {
+  return str.toLowerCase()
 }
 
-export function toUppercase(str: string = ''): string | undefined {
-    return str.toUpperCase();
+export function toUppercase(str: string = ""): string | undefined {
+  return str.toUpperCase()
 }
 
 export function convertArrayToString(arr: unknown[] | any) {
-    if (!arr || !Array.isArray(arr)) {
-        throw new Error('Invalid param type of the function');
-    }
+  if (!arr || !Array.isArray(arr)) {
+    throw new Error("Invalid param type of the function")
+  }
 
-    return JSON.stringify(arr);
+  return JSON.stringify(arr)
 }
